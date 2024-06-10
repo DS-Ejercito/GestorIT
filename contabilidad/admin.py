d747757e4cd2b57ccedee012f28e8df3bdeae328
Devm94 <116216613+Devm94@users.noreply.github.com>
2024-06-10 11:21:06 -0600

    Actualizacion 10062024

---
+++ contabilidad/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Movimiento
+from .models import Cierre_Diario
+
+admin.site.register(Movimiento)
+admin.site.register(Cierre_Diario)
+
+# Register your models here.
