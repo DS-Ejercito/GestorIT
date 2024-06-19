7b60d69f3874af33944671e71183de049f5a996d
Devm94 <116216613+Devm94@users.noreply.github.com>
2024-06-19 13:11:33 -0600

    Update settings.py

--- gestor_admin_it/settings.py
+++ gestor_admin_it/settings.py
@@ -95,6 +95,20 @@ DATABASES = {
                     },
     }
 }
+""" 
+DATABASES = {
+    'default': {
+        'ENGINE': 'mssql',
+        'NAME': 'gestor_admin_it',
+        'USER': 'sa',
+        'PASSWORD': 'T3cn0l0g1@13',
+        'HOST': '172.22.4.16',
+        'PORT': '',
+        'OPTIONS': {
+            'driver': 'ODBC Driver 17 for SQL Server',
+                    },
+    }
+} """
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
